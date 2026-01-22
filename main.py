681f6070430973609da91da8458ad9a432cf490b
Abdullah Yusuf Yıldırım <tt.shirted@gmail.com>
2026-01-22 13:54:33 +0300

    complete game logic for user_choice ==  rock choice scenarios

--- main.py
+++ main.py
@@ -8,8 +8,12 @@ while True:
     print(f"You chose {user_choice} computer chose {computer_choice}")
     if user_choice == computer_choice :
         print(f"Both players choce {user_choice} İt's a draw ")
+    elif user_choice == "Rock" :
+        if computer_choice == "Paper" :
+            print("Paper covers rock so computer win and  you are lose ")
+        elif computer_choice == "Scissor" :
+            print("Rock broke Scissor so  computer lose and you are win ")
 
-        break
 
 
 
